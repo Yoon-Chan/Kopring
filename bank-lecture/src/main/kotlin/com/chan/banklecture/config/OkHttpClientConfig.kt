d3feb34da157bc50bf4bba37d42fe2550288386a
윤찬 <vsvx13@naver.com>
2025-09-17 21:00:50 +0900

    [feat]: OkHttpClient 구성하기(GET, POST)

---
+++ bank-lecture/src/main/kotlin/com/chan/banklecture/config/OkHttpClientConfig.kt
@@ -0,0 +1,22 @@
+package com.chan.banklecture.config
+
+import okhttp3.OkHttpClient
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
+
+@Configuration
+class OkHttpClientConfig {
+
+    @Bean
+    fun httpClient(): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .followRedirects(true)
+            .followSslRedirects(true)
+            .build()
+    }
+}
