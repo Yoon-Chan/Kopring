da216c3cc2d7a1ed45e2775f93d240f6b0587858
윤찬 <vsvx13@naver.com>
2025-09-17 20:21:52 +0900

    [feat]: OAuth2 기본 구조 설정
    Config및 Oauth2 인터페이스 구성

---
+++ bank-lecture/src/main/kotlin/com/chan/banklecture/config/OAuth2Config.kt
@@ -0,0 +1,30 @@
+package com.chan.banklecture.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+/*
+    oauth2:
+        providers:
+            google:
+                client-id: ...
+                client-secret: ...
+                redirect-uri :...
+            github
+                client-id:...
+                ...
+*
+**/
+@Configuration
+@ConfigurationProperties("oauth2")
+class OAuth2Config {
+
+    val providers: MutableMap<String, Oauth2ProviderValues> = mutableMapOf()
+
+}
+
+data class Oauth2ProviderValues(
+    val clientId: String,
+    val clientSecret: String,
+    val redirectUri: String,
+)
