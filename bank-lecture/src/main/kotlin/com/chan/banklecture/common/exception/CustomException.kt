be8ef4c512444797018ecf83471111c257ac1481
윤찬 <vsvx13@naver.com>
2025-09-17 21:00:12 +0900

    [feat]: 커스텀 예외 처리 구성

---
+++ bank-lecture/src/main/kotlin/com/chan/banklecture/common/exception/CustomException.kt
@@ -0,0 +1,12 @@
+package com.chan.banklecture.common.exception
+
+class CustomException(
+    private val codeInterface: CodeInterface,
+    private val additionalMessage: String? = null
+) : RuntimeException(
+    if (additionalMessage == null) {
+        codeInterface.message
+    } else {
+        "${codeInterface.message} - $additionalMessage"
+    }
+)
