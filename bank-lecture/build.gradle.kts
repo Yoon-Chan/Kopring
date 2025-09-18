e657310933021ad78a4a880fd7788a91c6918ef5
윤찬 <vsvx13@naver.com>
2025-09-18 13:37:57 +0900

    [feat] Serialization plugin 추가

--- bank-lecture/build.gradle.kts
+++ bank-lecture/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
