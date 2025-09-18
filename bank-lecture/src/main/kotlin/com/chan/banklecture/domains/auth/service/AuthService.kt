ceeb98485f03046718c2f0d8deef6b0ab9c878e4
윤찬 <vsvx13@naver.com>
2025-09-18 14:37:05 +0900

    [feat]: Loggin/Transactional 관리 클래스 구현

--- bank-lecture/src/main/kotlin/com/chan/banklecture/domains/auth/service/AuthService.kt
+++ bank-lecture/src/main/kotlin/com/chan/banklecture/domains/auth/service/AuthService.kt
@@ -3,19 +3,26 @@ package com.chan.banklecture.domains.auth.service
 import com.chan.banklecture.common.exception.CustomException
 import com.chan.banklecture.common.exception.ErrorCode
 import com.chan.banklecture.common.jwt.JwtProvider
+import com.chan.banklecture.common.logging.Logging
 import com.chan.banklecture.interfaces.OauthServiceInterface
+import org.slf4j.Logger
 import org.springframework.stereotype.Service
+import com.chan.banklecture.common.transaction.Transactional
 
 @Service
 class AuthService(
     private val oAuth2ServiceInterface: Map<String, OauthServiceInterface>,
-    private val jwtProvider: JwtProvider
+    private val jwtProvider: JwtProvider,
+    private val logger: Logger = Logging.getLogger(AuthService::class.java),
+    val transactional: Transactional
 ) {
-    fun handleAuth(state: String, code: String): String {
+
+    fun handleAuth(state: String, code: String): String = Logging.logFor(logger) {
         //GOOGLE -> google
         val provider = state.lowercase()
 
-        val callService = oAuth2ServiceInterface[provider] ?: throw CustomException(ErrorCode.PROVIDER_NOT_FOUND, provider)
+        val callService =
+            oAuth2ServiceInterface[provider] ?: throw CustomException(ErrorCode.PROVIDER_NOT_FOUND, provider)
         val accessToken = callService.getToken(code)
         val userInfo = callService.getUserInfo(accessToken.accessToken)
         val token = jwtProvider.createToken(provider, userInfo.email, userInfo.name, userInfo.id)
