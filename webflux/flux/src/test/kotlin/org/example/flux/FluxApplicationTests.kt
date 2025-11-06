aeb0803b18b0103ac4545ca1b223a30847d2bbc6
윤찬 <vsvx13@naver.com>
2025-11-06 09:55:47 +0900

    [init] webflux관련 프로젝트 생성

---
+++ webflux/flux/src/test/kotlin/org/example/flux/FluxApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.flux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FluxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
